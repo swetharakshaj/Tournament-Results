73a77f26873737689d240b67b1cfd3cbc624c4bf
swetharakshaj <swetharaksha@gmail.com>
2015-08-31 23:33:29 -0700

    Update tournament.sql
    add comments to explain design of different tables

--- tournament.sql
+++ tournament.sql
@@ -6,14 +6,16 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-
+--players is designed to hold just the players registering and assigning them with unique ID
 CREATE TABLE players ( id SERIAL,
                        name TEXT );
 
+--matches is designed to give unique ID to every match played and stores the id of winner and loser
 CREATE TABLE matches ( matchid SERIAL,
                        winner INTEGER,
                        loser INTEGER );                      
 
+--scoreboard is designed to store the cumulative score of players along with their id and total number of matches played
 CREATE TABLE scoreboard ( player INTEGER,
                           score INTEGER,
                           matches INTEGER );
